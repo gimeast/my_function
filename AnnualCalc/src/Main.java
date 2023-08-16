13e3ef091123daf6ccbad5abfb1c4ceb9f313796
gimeast94 <dudu.gimeast@gmail.com>
2023-08-16 10:00:31 +0900

    회계연도 방식
    - 2년차 ~ 23년차 까지 연차계산

--- AnnualCalc/src/Main.java
+++ AnnualCalc/src/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
         // 예시 사용자의 입사일과 기준 일자 설정
-        LocalDate hireDate = LocalDate.of(2022, 2, 1);
-        LocalDate currentDate = LocalDate.of(2023, 12, 31);
+        LocalDate hireDate = LocalDate.of(2000, 1, 1);
+        LocalDate currentDate = LocalDate.now();
 
-        System.out.println("hireDate: " + hireDate);
-        System.out.println("currentDate: " + currentDate);
+        System.out.println("고용날짜: " + hireDate);
+        System.out.println("현재날짜: " + currentDate);
 
         float fiscalAnnual = 회계연도방식(hireDate, currentDate);
         System.out.println("회계연도 방식 연차계산 결과 : "+fiscalAnnual);
@@ -23,7 +23,12 @@ public class Main {
         int currentYear = currentDate.getYear(); // 현재연도
         int hireDateYear = hireDate.getYear(); // 고용연도
 
-        if(currentYear - hireDateYear < 2) { // 1년 미만자
+        int 근무년차 = currentYear - hireDateYear;
+        System.out.println("근무년차 : " + 근무년차);
+
+        int 기본년차 = 15;
+
+        if(근무년차 < 2) { // 1년 미만자
             System.out.println("1년 미만자");
 
             if (currentYear > hireDateYear) { // 해를 넘긴자 : 15 * (근무일수 / 365)
@@ -35,7 +40,7 @@ public class Main {
                 long days = ChronoUnit.DAYS.between(hireDate, LocalDate.of(hireDateYear, 12, 31))+1;
                 System.out.println("고용일 : " + (days) +"일");
 
-                result = 15 * ((float)days / 365);
+                result = 기본년차 * ((float)days / 365);
 
             } else { // 해를 안넘긴자 : 한달 만근시 1개의 월차가 생긴다.
                 System.out.println("고용일이 아직 년도를 넘지 않았습니다.");
@@ -47,6 +52,17 @@ public class Main {
                 result = period.getMonths();
             }
 
+        } else {
+            int[] 추가년차_범위 = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22};
+            int 추가년차 = 0;
+            for(int i = 0; i < 추가년차_범위.length; i++) {
+                if (근무년차 >= 추가년차_범위[i]) {
+                    추가년차 = i;
+                } else {
+                    break;
+                }
+            }
+            result = 기본년차 + 추가년차;
         }
 
         return result;
